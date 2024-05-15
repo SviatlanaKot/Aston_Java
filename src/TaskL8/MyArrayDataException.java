8cc6dabd11b64045f88b11988f95347860980970
SviatlanaKot <sve3363@gmail.com>
2024-05-15 20:42:30 +0200

    lesson_8

---
+++ src/TaskL8/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package TaskL8;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/TaskL8/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package TaskL8;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
